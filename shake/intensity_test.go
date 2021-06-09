f364e0cface8bf7eef7e9e6cd789190833b05ebe
Mark Chadwick <m.chadwick@gns.cri.nz>
2021-06-10 09:07:34 +1200

    add Moratalla2020 mmi calculation

--- shake/intensity_test.go
+++ shake/intensity_test.go
@@ -12,22 +12,23 @@ func TestIntensity(t *testing.T) {
 		i int32   // ShakeMap
 		w int32   // WaldQuitorianoHeatonKanamori1999
 		f int32   // FaenzaMichelini2010
+		p int32   // Moratalla2020
 	}{
 		// original tests
-		{0.01 / 100.0, 1, 1, 1},
-		{0.09 / 100.0, 2, 1, 2},
-		{1.9 / 100.0, 5, 3, 5},
-		{5.8 / 100.0, 6, 4, 6},
-		{11.0 / 100.0, 7, 5, 7},
-		{22.0 / 100.0, 8, 7, 8},
-		{43.0 / 100.0, 8, 8, 8},
-		{83.0 / 100.0, 9, 9, 9},
-		{161.0 / 100.0, 10, 10, 10},
+		{0.01 / 100.0, 1, 1, 1, 1},
+		{0.09 / 100.0, 2, 1, 2, 2},
+		{1.9 / 100.0, 5, 3, 5, 4},
+		{5.8 / 100.0, 6, 4, 6, 5},
+		{11.0 / 100.0, 7, 5, 7, 5},
+		{22.0 / 100.0, 8, 7, 8, 7},
+		{43.0 / 100.0, 8, 8, 8, 8},
+		{83.0 / 100.0, 9, 9, 9, 9},
+		{161.0 / 100.0, 10, 10, 10, 10},
 		// bounds tests
-		{0.0, 1, 1, 1},
-		{-0.09 / 100.0, 2, 1, 2},
-		{-100.0 / 100.0, 1, 9, 9},
-		{1.0e+10, 12, 12, 12},
+		{0.0, 1, 1, 1, 1},
+		{-0.09 / 100.0, 2, 1, 2, 2},
+		{-100.0 / 100.0, 1, 9, 9, 9},
+		{1.0e+10, 12, 12, 12, 12},
 	}
 
 	for _, intensity := range intensities {
@@ -36,6 +37,9 @@ func TestIntensity(t *testing.T) {
 		}
 		if i := Intensity(FaenzaMichelini2010{}, intensity.v); i != intensity.f {
 			t.Errorf("invalid FaenzaMichelini2010 rawintensity [%g cm/s]: %d (calculated) != %d (expected)", 100.0*intensity.v, i, intensity.f)
+		}
+		if i := Intensity(Moratalla2020{}, intensity.v); i != intensity.p {
+			t.Errorf("invalid Moratalla2020 rawintensity [%g cm/s]: %d (calculated) != %d (expected)", 100.0*intensity.v, i, intensity.p)
 		}
 	}
 }
