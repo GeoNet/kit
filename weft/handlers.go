821713ce30b6cd1d254d629e2dd6d8c87370d8c0
Callum Morris <c.morris@gns.cri.nz>
2022-08-22 15:46:01 +1200

    Remove strict CSP as it is unused

--- weft/handlers.go
+++ weft/handlers.go
@@ -71,20 +71,6 @@ var defaultCsp = map[string]string{
 	"object-src":      "'none'",
 }
 
-var strictCsp = map[string]string{
-	"default-src":     "'none'",
-	"img-src":         "'self'",
-	"font-src":        "'none'",
-	"style-src":       "'none'",
-	"script-src":      "'none'",
-	"connect-src":     "'none'",
-	"frame-src":       "'none'",
-	"form-action":     "'none'",
-	"base-uri":        "'none'",
-	"frame-ancestors": "'none'",
-	"object-src":      "'none'",
-}
-
 /**
  * RequestHandler should write the response for r into b and adjust h as required
  */
@@ -551,16 +537,6 @@ func Soh(r *http.Request, h http.Header, b *bytes.Buffer) error {
 func ReturnDefaultCSP() map[string]string {
 	copy := make(map[string]string)
 	for k, v := range defaultCsp {
-		copy[k] = v
-	}
-	return copy
-}
-
-// ReturnStrictCSP returns the strict Content Security Policy used by
-// error handlers. This is a copy of the map, so can be changed safely if needed.
-func ReturnStrictCSP() map[string]string {
-	copy := make(map[string]string)
-	for k, v := range strictCsp {
 		copy[k] = v
 	}
 	return copy
